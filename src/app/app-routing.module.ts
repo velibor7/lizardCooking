0379165a5889b0ee58821eee01119213256b0ad6
Velibor <veliborvasiljevic7@gmail.com>
2020-04-30 15:08:26 +0200

    basiccc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MyRecipesComponent } from "./my-recipes/my-recipes.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: MyRecipesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
